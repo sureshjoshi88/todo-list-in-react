6c4be643dfd83416781b8516f3ff2f4ca58df21b
sureshjoshi88 <sj3795789@gmail.com>
2025-06-06 11:49:33 +0530

    todo-list

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import TodoList from './component/TodoList'
+
+function App() {
+  return (
+   <>
+    <TodoList/>
+   </>
+  )
+}
+
+export default App
---
+++ src/component/TodoList.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+
+const TodoList = () => {
+
+    const [array, setArray] = useState([]);
+    const [task, setTask] = useState('')
+    const [filter, setFilter] = useState('All');
+    const addTaskButton = () => {
+        if (task === "") {
+            alert("please enter value");
+        } else {
+            const newTodo = {
+                text: task,
+                checked: false
+            }
+            setArray([...array, newTodo]);
+            setTask("");
+
+        }
+    }
+
+    const deleteTask = (index) => {
+        const finalList = array.filter((item, ind) => ind !== index);
+
+        setArray(finalList);
+    }
+    const toggleCheckbox = (index) => {
+        const updastedArray = array.map((item, ind) => {
+            return ind === index ? { ...item, checked: !item.checked } : item;
+        });
+        setArray(updastedArray)
+    }
+    const handleFilter = (e) => {
+        setFilter(e.target.value)
+    }
+    const filterArray = array.filter((item) => {
+        if (filter === "All") return true;
+        if (filter === "Active") return !item.checked;
+        if (filter === "Compalte") return item.checked;
+        return true;
+
+    })
+
+    return (
+        <>
+            <div className='p-1'>
+                <nav className='flex justify-between p-2'>
+                <p className='font-bold mt-2 text-2xl '>Made By Suresh Joshi</p>
+                <p className='font-bold mt-2 text-2xl'>Todo app</p>
+                <button>theame</button>
+                </nav>
+                <div className='text-end p-2 mt-2'>
+                    <select className='border rounded' value={filter} onChange={handleFilter} name="" id="">
+                        <option value="All">All</option>
+                        <option value="Active">Active</option>
+                        <option value="Compalte">Compalte</option>
+                    </select>
+                </div>
+                <div className='flex justify-center mt-10 gap-2'>
+                    <input className='border rounded' value={task} onChange={(e) => setTask(e.target.value)} type="text" id='101' placeholder='Enter a task' required />
+                    <button onClick={addTaskButton} className='bg-green-500 p-1 ps-3 pe-3 rounded cursor-pointer'>Add task</button>
+                </div>
+            </div>
+
+            {filterArray.map((value, index) =>
+                <div className='grid md:grid-cols-3 p-2'>
+                    <div className='w-10'>
+                        <input type="checkbox" checked={value.checked} onChange={() => toggleCheckbox(index)} name="" id="" />
+                    </div>
+                    <div className='w-70'>
+                        <p className='text-xl font-medium'>{value.text}</p>
+                    </div>
+                    <div className='w-20 flex gap-3'>
+                        <button className='bg-amber-300 p-1 ps-2 pe-2 rounded cursor-pointer'>Edit</button>
+                        <button onClick={() => deleteTask(index)} className='bg-red-600 p-1 ps-2 pe-2 text-white rounded cursor-pointer'>Delete</button>
+                    </div>
+                    
+                </div>
+            )}
+        </>
+    )
+}
+
+export default TodoList
