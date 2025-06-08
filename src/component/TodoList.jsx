507a8d6b7dd18f04f2e2ae8d966c08bd7e591079
sureshjoshi88 <sj3795789@gmail.com>
2025-06-08 15:46:37 +0530

    fixed bugs

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -10,7 +10,14 @@ const TodoList = (props) => {
     const [task, setTask] = useState('')
     const [filter, setFilter] = useState('All');
 
-   
+
+       useEffect(() => {
+        const storedata = JSON.parse(localStorage.getItem("todo")) || [];
+        setArray(storedata);
+
+    }, []);
+
+
     const addTaskButton = () => {
         if (task.trim() === "") {
             alert("please enter value");
@@ -19,34 +26,23 @@ const TodoList = (props) => {
                 text: task,
                 checked: false
             }
-            setArray([...array, newTodo]);
+            const finalarray = [...array, newTodo]
+            setArray(finalarray);
             setTask("");
-        localStorage.setItem("todo",JSON.stringify(array));
-  
+            localStorage.setItem("todo", JSON.stringify(finalarray));
         }
         
     }
-  
-     useEffect(()=>{
-        const storedata = JSON.parse(localStorage.getItem("todo"))||[];
-                setArray(storedata)
-            
-    
-    },[]);
 
-   
-    
-   
-
-     const keypresEvent=(e)=>{
-        if(e.key==="Enter"){
-            addTaskButton();            
+    const keypresEvent = (e) => {
+        if (e.key === "Enter") {
+            addTaskButton();
         }
-     }
-    
+    }
+
     const deleteTask = (index) => {
         const finalList = array.filter((item, ind) => ind !== index);
-        localStorage.setItem('todo',JSON.stringify(finalList));
+        localStorage.setItem('todo', JSON.stringify(finalList));
         setArray(finalList);
 
     }
@@ -54,7 +50,7 @@ const TodoList = (props) => {
         const updastedArray = array.map((item, ind) => {
             return ind === index ? { ...item, checked: !item.checked } : item;
         });
-                localStorage.setItem('todo',JSON.stringify(updastedArray));
+        localStorage.setItem('todo', JSON.stringify(updastedArray));
         setArray(updastedArray)
     }
     const handleFilter = (e) => {
@@ -68,23 +64,23 @@ const TodoList = (props) => {
 
     })
 
-    const handleEditTask = (index) =>{
+    const handleEditTask = (index) => {
         const newTask = prompt("enter a value")
-        if(newTask!==null&& newTask.trim()!==""){
-            const editTask = array.map((item,ind)=> ind===index?{...item,text:newTask}:item)
-            localStorage.setItem('todo',JSON.stringify(editTask));
+        if (newTask !== null && newTask.trim() !== "") {
+            const editTask = array.map((item, ind) => ind === index ? { ...item, text: newTask } : item)
+            localStorage.setItem('todo', JSON.stringify(editTask));
             setArray(editTask)
-        }        
+        }
     }
     return (
         <>
             <div className='p-1'>
                 <nav className='flex justify-between p-2'>
-                <p className='font-bold mt-2 text-2xl '>Made By Suresh Joshi</p>
-                <p className='font-bold mt-2 text-2xl'>Todo app</p>
-                {props.mode==="light"?
-                  <button className='cursor-pointer text-2xl' onClick={()=>props.handlemode()}><BsMoonFill /></button>:<button className='cursor-pointer text-2xl'  onClick={()=>props.handlemode()}><IoIosSunny /></button>
-              }
+                    <p className='font-bold mt-2 text-2xl '>Made By Suresh Joshi</p>
+                    <p className='font-bold mt-2 text-2xl'>Todo app</p>
+                    {props.mode === "light" ?
+                        <button className='cursor-pointer text-2xl' onClick={() => props.handlemode()}><BsMoonFill /></button> : <button className='cursor-pointer text-2xl' onClick={() => props.handlemode()}><IoIosSunny /></button>
+                    }
                 </nav>
                 <div className='text-end p-2 mt-2'>
                     <select className='border rounded' value={filter} onChange={handleFilter} name="" id="">
@@ -100,25 +96,25 @@ const TodoList = (props) => {
             </div>
 
             {filterArray.map((value, index) =>
-               <div key={index}>
-                 <div className='grid md:grid-cols-4 p-2' >
-                    <div>
-                        <p className='text-xl font-medium'>{index+1}</p>
-                    </div>
-                    <div>
-                        <input type="checkbox" className=' h-8 w-4' checked={value.checked} onChange={() => toggleCheckbox(index)} name="" id="" />
-                    </div>
-                    <div >
-                        <p className='text-xl font-medium'>{value.text}</p>
-                    </div>
-                    <div className='flex gap-3'>
-                        <button className='bg-amber-300 p-1 ps-2 pe-2 rounded cursor-pointer font-medium' onClick={()=>handleEditTask(index)}>Edit</button>
-                        <button onClick={() => deleteTask(index)} className='bg-red-600 p-1 ps-2 pe-2 text-white rounded cursor-pointer font-medium'>Delete</button>
-                    </div>
+                <div key={index}>
+                    <div className='grid md:grid-cols-4 p-2' >
+                        <div>
+                            <p className='text-xl font-medium'>{index + 1}</p>
+                        </div>
+                        <div>
+                            <input type="checkbox" className=' h-8 w-4' checked={value.checked} onChange={() => toggleCheckbox(index)} name="" id="" />
+                        </div>
+                        <div >
+                            <p className='text-xl font-medium'>{value.text}</p>
+                        </div>
+                        <div className='flex gap-3'>
+                            <button className='bg-amber-300 p-1 ps-2 pe-2 rounded cursor-pointer font-medium' onClick={() => handleEditTask(index)}>Edit</button>
+                            <button onClick={() => deleteTask(index)} className='bg-red-600 p-1 ps-2 pe-2 text-white rounded cursor-pointer font-medium'>Delete</button>
+                        </div>
 
+                    </div>
+                    <hr />
                 </div>
-                <hr />
-               </div>
             )}
         </>
     )
